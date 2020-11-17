587d61b0aff3a589206792fefe58e30002d99474
VFerrari <vferrari@mpc.com.br>
2020-11-17 15:35:38 -0300

    Adding penalty for infeasible solution

---
+++ src/problems/qbfpt/solvers/GA_QBFPT.java
@@ -0,0 +1,102 @@
+package problems.qbfpt.solvers;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Set;
+
+import metaheuristics.ga.AbstractGA.Chromosome;
+import problems.qbf.solvers.GA_QBF;
+import problems.qbfpt.QBFPT;
+import solutions.Solution;
+
+
+/**
+ * Metaheuristic GA (Genetic Algorithm) for obtaining an optimal solution 
+ * to a QBFPT (Quadractive Binary Function  with Prohibited Triples-- {@link QBFPT}). 
+ *
+ * @author rsaraiva, sipamplona, vferrari
+ */
+public class GA_QBFPT extends GA_QBF {
+	
+    /**
+     * The set T of prohibited triples.
+     */
+    private final Set<List<Integer>> T;
+
+	/**
+	 * Constructor for the GA_QBF class. The QBF objective function is passed as
+	 * argument for the superclass constructor.
+	 * 
+	 * @param generations
+	 *            Maximum number of generations.
+	 * @param popSize
+	 *            Size of the population.
+	 * @param mutationRate
+	 *            The mutation rate.
+	 * @param filename
+	 *            Name of the file for which the objective function parameters
+	 *            should be read.
+	 * @throws IOException
+	 *             Necessary for I/O operations.
+	 */
+	public GA_QBFPT(Integer generations, Integer popSize, Double mutationRate, String filename) throws IOException {
+		super(generations, popSize, mutationRate, filename);
+		
+        // Instantiate QBFPT problem, store T and update objective reference.
+        QBFPT qbfpt = new QBFPT(filename);
+        T = qbfpt.getT();
+        ObjFunction = qbfpt;
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * Adds penalty if solution is infeasible.
+	 */
+	@Override
+	protected Double fitness(Chromosome chromosome) {
+		Solution<Integer> sol = decode(chromosome);
+				
+		return sol.cost - (isSolutionFeasible(sol) ? 0 : 1000000);
+	}
+	
+	/**
+	 * Check if any triple restriction is violated.
+	 */
+    private boolean isSolutionFeasible(Solution<Integer> sol) {
+    	boolean feasible = true;
+        Integer e1, e2, e3;
+                
+    	for (List<Integer> t : T) {
+
+            /**
+             * Detach elements from (e1, e2, e3). They are stored as numbers 
+             * from [0, n-1] in sol., different than in T ([1, n]).
+             */
+            e1 = t.get(0) - 1;
+            e2 = t.get(1) - 1;
+            e3 = t.get(2) - 1;
+
+            // e1, e2 and e3 in solution -> infeasible.
+            if (sol.contains(e1) && sol.contains(e2) && sol.contains(e3)) {
+            	feasible = false;
+            	break;
+            }
+        }
+    	
+    	return feasible;
+    }
+	
+	/**
+     * A main method used for testing the GA metaheuristic.
+     */
+	public static void main(String[] args) throws IOException {
+		long startTime = System.currentTimeMillis();
+		GA_QBFPT ga = new GA_QBFPT(1000, 100, 1.0 / 100.0, "instances/qbf060");
+		Solution<Integer> bestSol = ga.solve(1800.0);
+		System.out.println("maxVal = " + bestSol);
+		long endTime = System.currentTimeMillis();
+		long totalTime = endTime - startTime;
+		System.out.println("Time = " + (double) totalTime / (double) 1000 + " seg");
+	}
+}
